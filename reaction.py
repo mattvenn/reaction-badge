df58ffe8bb48931ed4ccd83d6cc1e18deaf7d90c
Matt Venn <matt@mattvenn.net>
2014-02-22 11:57:07 +0000

    added raspi bits

--- reaction.py
+++ reaction.py
@@ -1,11 +1,35 @@
+"""
+title: reaction timer badge
+author: matt venn, 2014
+license: GPL attribution share alike
+"""
 import time
 import random
 import csv
 
 csv_file = 'scores.csv'
 
+#undefine for testing with GPIO lib
+raspi = False
+
+#interrupt does nothing
+def gpio_callback(gpio_id, val):
+    pass
+
+if raspi:
+    import RPi.GPIO as GPIO
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    button = 18
+    RPIO.add_interrupt_callback(button, gpio_callback, pull_up_down=RPIO.PUD_UP)
+
 def wait_button():
-    raw_input("press button")
+    if raspi:
+        #wait for interrupt
+        RPIO.wait_for_interrupts()
+    else:
+        #wait for key press
+        raw_input("press enter key")
 
 #write the score
 def save_score(score):
@@ -38,29 +62,42 @@ def read_file():
         return []
 
 
-while True:
-    #wait for button to start game
-    wait_button()
+if __name__ == '__main__':
+    while True:
+        #wait for button to start game
+        wait_button()
 
-    #get a random time to wait
-    random_wait = 0 #random.randint(3,10)
+        #get a random time to wait
+        random_wait = random.randint(3,10)
 
-    #wait it
-    for i in range(random_wait):
-        time.sleep(1)
-        print(".")
+        #wait it
+        for i in range(random_wait):
+            time.sleep(1)
+            if raspi:
+                pass
+            else:
+                print(".")
 
-    #go!
-    print("go!")
-    start_time = time.time()
+        #go!
+        if raspi:
+            pass
+        else:
+            print("go!")
+        start_time = time.time()
 
-    wait_button()
+        #wait for button
+        wait_button()
 
-    reaction_time = time.time() - start_time
-    short_time = '%.2f' % reaction_time
-    print("you got", short_time)
-    pos = get_pos(reaction_time)
-    print("you came", pos)
-    save_score(reaction_time)
+        #work out reaction time and get position in high scores
+        reaction_time = time.time() - start_time
+        short_time = '%.2f' % reaction_time
+        pos = get_pos(reaction_time)
 
+        #save high score
+        save_score(reaction_time)
 
+        if raspi:
+            pass
+        else:
+            print("you got", short_time)
+            print("you came", pos)
